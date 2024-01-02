f21647d0d4c6ab4635494a505c0233056d262d56
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-02 18:47:56 -0300

    Finalização método receberDados()

--- br/com/calculator/Calculadora.java
+++ br/com/calculator/Calculadora.java
@@ -3,9 +3,15 @@ package br.com.calculator;
 import javax.swing.*;
 
 public class Calculadora {
-     void receberDados() {
+    void receberDados() {
         String numeroDeValores = JOptionPane.showInputDialog(null, "Com quantos numeros você deseja operar?");
         int numeroDeValoresConvertido = Integer.parseInt(numeroDeValores);
         int[] valores = new int[numeroDeValoresConvertido];
+
+        for (int i = 0; i < numeroDeValoresConvertido; i++) {
+            String valor = JOptionPane.showInputDialog(null, String.format("Digite o %d° valor", i+1));
+            int valorConvetido = Integer.parseInt(valor);
+            valores[i] = valorConvetido;
+        }
     }
 }
