33bc228bf3fb7975a53b80003155c485e8f83497
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-03 05:24:09 -0300

    Finalização método adicao() da classe Operações

--- br/com/calculator/Operacoes.java
+++ br/com/calculator/Operacoes.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 
 public class Operacoes {
     void adicao(double a, Calculadora calculadora) {
-        double soma;
+        double soma = 0;
         int newA = (int) a;
-        JOptionPane.showMessageDialog(null, String.format("Você irá operar com %d valores.", newA));
         double[] valoresAOperar = calculadora.valores;
-        for (int i = 0; i <valoresAOperar.length; i++) {
-            System.out.println(valoresAOperar[i]);
+        for (int i = 0; i < valoresAOperar.length; i++) {
+            soma += valoresAOperar[i];
         }
+        JOptionPane.showMessageDialog(null, String.format("Resultado da operação: %.1f", soma));
     }
 }
