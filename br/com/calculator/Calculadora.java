169094e0c4da21aac2a79fdab4f886ed4b5a9a95
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-03 05:20:07 -0300

    Alterações na classe Calculadora e implementação no método adicao()

--- br/com/calculator/Calculadora.java
+++ br/com/calculator/Calculadora.java
@@ -4,17 +4,19 @@ import javax.swing.*;
 
 public class Calculadora {
     double[] valores;
+    double numeroDeValoresConvertido;
+
     void receberDados() {
         String numeroDeValores = JOptionPane.showInputDialog(null, "Com quantos numeros você deseja operar?");
-        double numeroDeValoresConvertido = Double.parseDouble(numeroDeValores);
-        double[] valores = new double[(int) numeroDeValoresConvertido];
+        numeroDeValoresConvertido = Double.parseDouble(numeroDeValores);
+        valores = new double[(int) numeroDeValoresConvertido];
 
         for (int i = 0; i < numeroDeValoresConvertido; i++) {
             String valor = JOptionPane.showInputDialog(null, String.format("Digite o %d° valor", i + 1));
             double valorConvetido = Double.parseDouble(valor);
             valores[i] = valorConvetido;
         }
-        String[] options = { "Adição", "Subtração", "Multiplicação", "Divisão" };
+        String[] options = {"Adição", "Subtração", "Multiplicação", "Divisão"};
         int x = JOptionPane.showOptionDialog(
                 null,
                 "Qual operação você deseja fazer?",
@@ -22,12 +24,12 @@ public class Calculadora {
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.INFORMATION_MESSAGE,
                 null,
-                options,options[0]
+                options, options[0]
         );
-        switch (x){
+        switch (x) {
             case 0:
                 Operacoes operar = new Operacoes();
-                operar.adicao(numeroDeValoresConvertido);
+                operar.adicao(numeroDeValoresConvertido, this);
         }
     }
 }
--- br/com/calculator/Operacoes.java
+++ br/com/calculator/Operacoes.java
@@ -3,7 +3,13 @@ package br.com.calculator;
 import javax.swing.*;
 
 public class Operacoes {
-void adicao(double a) {
-
-}
+    void adicao(double a, Calculadora calculadora) {
+        double soma;
+        int newA = (int) a;
+        JOptionPane.showMessageDialog(null, String.format("Você irá operar com %d valores.", newA));
+        double[] valoresAOperar = calculadora.valores;
+        for (int i = 0; i <valoresAOperar.length; i++) {
+            System.out.println(valoresAOperar[i]);
+        }
+    }
 }
